b5e0d3e38fafe7c27198e783c854ad4e13b98c1d
Lukas Meyer <lumeyer@stud.hs-heilbronn.de>
2017-01-01 01:06:40 +0100

    solve problem 26

--- PE/src/Main.java
+++ PE/src/Main.java
@@ -4,7 +4,7 @@ import pe_1_25.*;
 public class Main {
 
 	public static void main(String[] args) {
-		new PE28();
+		new PE26();
 		//new P15();		
 	}
 
--- PE/src/pe_26_50/PE26.java
+++ PE/src/pe_26_50/PE26.java
@@ -1,11 +1,56 @@
 package pe_26_50;
 
-public class PE26 {
-	public PE26() {
-		for (int i = 0; i <= 1000; i++) {
-			for (int j = 0; j <= 1000; j++) {
+import java.util.ArrayList;
 
+public class PE26 {
+	
+	int longestProducer = 0;
+	int longestCycle = 0;
+	int cnt = 0;
+	
+	public PE26() {
+		for (int i = 1; i < 1000; i++) {
+			computeCycle(i);
+		}
+		System.out.println();
+		System.out.println("Longest Cycle: " + longestCycle);
+		System.out.println("Longest Producer: " + longestProducer);
+	}
+	
+	private void computeCycle(int divisor) {
+		int begin = 10;
+		boolean noCycleDetected = true;
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		
+		while(noCycleDetected) {
+			if (list.contains(begin)) {
+				noCycleDetected = false;
+				int curCycleLength = list.size() - searchIndexOf(list, begin, divisor);
+				if (curCycleLength > longestCycle) {
+					longestCycle = curCycleLength;
+					longestProducer = divisor;
+				}
+			}
+			
+			list.add(begin);
+			begin = begin - ((begin/divisor) * divisor);
+			begin *= 10;
+			
+			if (begin == 0) {
+				return;
 			}
 		}
 	}
+	
+	
+
+	private int searchIndexOf(ArrayList<Integer> list, int begin, int j) {
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i) == begin) {
+				return i;
+			}
+		}
+		return 0;
+	}
+
 }
